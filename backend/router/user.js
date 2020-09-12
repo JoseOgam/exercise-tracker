b2de6a0258addc4da5e25966b469ffa54a57978c
JoseOgam <jotienoochieng@gmail.com>
2020-09-12 19:27:06 -0400

    setting up user routers in the backend

--- backend/router/user.js
+++ backend/router/user.js
@@ -17,4 +17,14 @@ router.post("/users", (req, res) => {
       res.status(400).json("Error:" + err);
     });
 });
+
+router.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(400).json("Error:" + err);
+    });
+});
 module.exports = router;
